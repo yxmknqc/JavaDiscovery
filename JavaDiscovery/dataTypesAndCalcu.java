65a8ca71353f8c0f2ad859e28c89a61358be5513
yxmknqc <59868670+yxmknqc@users.noreply.github.com>
2020-06-25 20:47:18 +0800

    Add files via upload

---
+++ JavaDiscovery/dataTypesAndCalcu.java
@@ -0,0 +1,18 @@
+public class dataTypesAndCalcu {
+	static int a=5, b=6;
+    static long c=2200000000l;
+    static long d=2100000000;
+    static byte e=(byte)132;
+    static char c_a = '@';
+    static char c_b =9733;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        //System.out.println(String.valueOf(a+b));
+        //System.out.println(String.valueOf(a-b));
+        //System.out.printc_ln(String.valueOf(a*b));
+        //System.out.println(String.valueOf(a/b));
+        //System.out.println(String.valueOf(a%b));
+        //System.out.println(String.valueOf(c_a+c_b));
+        //System.out.println(e);
+    }
+}
---
+++ JavaDiscovery/hellojava.java
@@ -0,0 +1,11 @@
+public class hellojava {
+    static int a=5, b=6;
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        System.out.println(String.valueOf(a+b));
+        System.out.println(String.valueOf(a-b));
+        System.out.println(String.valueOf(a*b));
+        System.out.println(String.valueOf(a/b));
+        System.out.println(String.valueOf(a%b));
+    }
+}
